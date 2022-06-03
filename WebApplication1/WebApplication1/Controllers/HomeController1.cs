561abfe34843becc711ac3085ac09ea533e76925
rahulkumar22 <rahul.kumar@nagarro.com>
2022-06-03 09:18:16 +0530

    Add Actions

---
+++ WebApplication1/WebApplication1/Controllers/HomeController1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "these are all books";
+        }
+        public string GetBook(int id)
+        {
+            return $"book with id {id}";
+        }
+
+    }
+}
